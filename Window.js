58500b97f45dcabbfeeec1e57e7b7a0263ab4e3a
Zulficar Hasan Joy <54574546+zulficarjoy@users.noreply.github.com>
2020-08-31 02:43:53 +0600

    Update Window.js

--- Window.js
+++ Window.js
@@ -1,14 +1,10 @@
 'use strict'
 
 const { BrowserWindow } = require('electron')
-
-// default window settings
 const defaultProps = {
   width: 500,
   height: 800,
   show: false,
-  
-  // update for electron V5+
   webPreferences: {
     nodeIntegration: true
   }
@@ -21,9 +17,6 @@ class Window extends BrowserWindow {
 
     // load the html and open devtools
     this.loadFile(file)
-    // this.webContents.openDevTools()
-
-    // gracefully show when ready to prevent flickering
     this.once('ready-to-show', () => {
       this.show()
     })
